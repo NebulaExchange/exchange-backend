c260744400fed145484ea3b5c193b632d9a7b2b8
Filip <filip.vostatek@gmail.com>
2025-05-21 23:01:05 +0100

    cors

--- trading/src/app.ts
+++ trading/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import apiRoutes from './routes/api';
 
 const app: Application = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
