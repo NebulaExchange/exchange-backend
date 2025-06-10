56bdd97a801e36b176baa8abfb2ffe19114a5517
Filip <filip.vostatek@gmail.com>
2025-06-10 12:41:42 +0100

    removing console log

--- trading/src/index.ts
+++ trading/src/index.ts
@@ -8,5 +8,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`📚 Swagger UI available at http://localhost:${PORT}/`);
-  console.log(`📋 Swagger JSON available at http://localhost:${PORT}/swagger.json`);
 });
